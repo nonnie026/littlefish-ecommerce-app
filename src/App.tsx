b5e64362e0a54126ff09082c0645c2e61ada958b
Nonnie <41764381+nonnie026@users.noreply.github.com>
2023-12-17 16:59:44 +0200

    chore: setup basic react store and react-query provider (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Grid, Typography } from '@mui/material'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Grid container minHeight={'100vh'}>
-      <Grid item xs={true}>
-        <Typography variant="h1">Our Commerce</Typography>
+    <QueryClientProvider client={queryClient}>
+      <Grid container minHeight={'100vh'}>
+        <Grid item xs={true}>
+          <Typography variant="h1">Our Commerce</Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </QueryClientProvider>
   )
 }
 
