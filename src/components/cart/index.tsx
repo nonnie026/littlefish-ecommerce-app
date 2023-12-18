7fc8f4604949d98e002f93c7715bcbdbb7a878ee
Nondumiso Ngcobo <ngcobonondumiso27@gmail.com>
2023-12-18 15:32:17 +0200

    style: add padding and correct widths in the cart

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -28,11 +28,24 @@ export const Cart = ({
         horizontal: 'left'
       }}
     >
-      <Box overflow="auto" width={310}>
-        <Typography variant="h6">My Cart</Typography>
-        <Typography color="GrayText" variant="subtitle1">
-          {`${numberOfItems} ${numberOfItems === 1 ? 'item' : 'items'} in cart`}
-        </Typography>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent={'center'}
+        alignItems={'center'}
+        overflow="auto"
+        gap={2}
+        width={320}
+        p={2}
+      >
+        <Box textAlign={'center'}>
+          <Typography variant="h6">My Cart</Typography>
+          <Typography color="GrayText" variant="subtitle1">
+            {`${numberOfItems} ${
+              numberOfItems === 1 ? 'item' : 'items'
+            } in cart`}
+          </Typography>
+        </Box>
         {cartItems?.map((item) => (
           <Grid
             key={item.id}
@@ -40,12 +53,16 @@ export const Cart = ({
             gap={2}
             justifyContent={'space-between'}
             alignItems={'center'}
+            py={1}
             borderBottom={'1px solid #ccc'}
           >
             <Typography variant="body1">1 x</Typography>
-            <Box component="img" src={item.image} width={50} height={50} />
-            <TruncatedText text={item.title} minStringLength={30} />
+            <Box component="img" src={item.image} width={65} height={65} />
+            <Box width={150}>
+              <TruncatedText text={item.title} minStringLength={30} />
+            </Box>
             <CancelOutlined
+              fontSize="small"
               onClick={() => handleRemoveFromCart(item.id)}
               style={{ cursor: 'pointer' }}
             />
