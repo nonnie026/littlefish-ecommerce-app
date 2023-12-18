47dca73b1290d5c48040ff2634c59585e5ca080f
Nondumiso Ngcobo <ngcobonondumiso27@gmail.com>
2023-12-18 15:50:20 +0200

    style: add logo image more padding to the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,21 @@
 // src/components/Header.tsx
 import { ShoppingCart } from '@mui/icons-material'
 import { Box, IconButton, Typography } from '@mui/material'
+import logo from '../../assets/logo.svg'
 import { IProps } from './interface'
 
 export const Header = ({ onCartClick }: IProps) => (
   <Box
     component="header"
-    py={2}
+    py={4}
     display="flex"
     justifyContent="space-between"
     alignItems="center"
     maxWidth="lg"
     mx="auto"
   >
-    <Box display="flex" gap={2}>
-      <Box component="img" src={require('../../assets/logo.svg')} />
+    <Box display="flex" gap={3}>
+      <Box component="img" src={logo} />
       <Typography variant="h6">OurCommerce</Typography>
     </Box>
     <IconButton color="primary" aria-label="cart" onClick={onCartClick}>
