91d66b275d432f7d185e2fe62d96dbcd83e51b49
Nondumiso Ngcobo <ngcobonondumiso27@gmail.com>
2023-12-18 16:26:23 +0200

    style: align the add to cart button to the bottom

--- src/components/products-list/index.tsx
+++ src/components/products-list/index.tsx
@@ -29,65 +29,72 @@ export const ProductsList = ({ products, handleAddToCart }: IProps) => {
         >
           <Card>
             <Box
-              fontSize={10}
-              py={0.875}
-              pr={0.625}
-              pl={1}
               display="flex"
-              alignItems="center"
-              gap={0.5}
+              flexDirection="column"
+              justifyContent="space-between"
+              height="100%"
             >
-              <CheckCircle fontSize="inherit" color="success" />
-              <Typography variant="inherit" color="InfoText">
-                in stock
-              </Typography>
-            </Box>
-            <Box
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              position="relative"
-            >
-              <CardMedia
-                component="img"
-                image={product.image}
-                alt={product.title}
-                sx={{ objectFit: 'contain', width: 150, height: 150 }}
-              />
-            </Box>
-            <CardContent sx={{ px: 0, py: 2 }}>
-              <Box alignItems="center" display="flex" mb={2} gap={1.25}>
-                <Rating
-                  name="read-only"
-                  size="small"
-                  value={product.rating.rate}
-                  readOnly
-                />
-                <Typography variant="caption" color="GrayText">
-                  Reviews ({product.rating.count})
-                </Typography>
-              </Box>
-              <TruncatedText text={product.title} minStringLength={60} />
-            </CardContent>
-            <CardActions sx={{ p: 0 }}>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent={'space-between'}
-                flexGrow={1}
-              >
-                <Typography variant="body2" color="textPrimary">
-                  ${product.price}
-                </Typography>
-                <Button
-                  onClick={() => handleAddToCart(product)}
-                  size="small"
-                  color="primary"
+              <Box>
+                <Box
+                  fontSize={10}
+                  py={0.875}
+                  pr={0.625}
+                  pl={1}
+                  display="flex"
+                  alignItems="center"
+                  gap={0.5}
                 >
-                  Add To Cart
-                </Button>
+                  <CheckCircle fontSize="inherit" color="success" />
+                  <Typography variant="inherit">in stock</Typography>
+                </Box>
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  position="relative"
+                >
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    alt={product.title}
+                    sx={{ objectFit: 'contain', width: 150, height: 150 }}
+                  />
+                </Box>
+                <CardContent sx={{ px: 0, py: 2 }}>
+                  <Box alignItems="center" display="flex" mb={2} gap={1.25}>
+                    <Rating
+                      name="read-only"
+                      size="small"
+                      value={product.rating.rate}
+                      readOnly
+                    />
+                    <Typography variant="caption" color="GrayText">
+                      Reviews ({product.rating.count})
+                    </Typography>
+                  </Box>
+                  <TruncatedText text={product.title} minStringLength={60} />
+                </CardContent>
               </Box>
-            </CardActions>
+              <CardActions sx={{ p: 0 }}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent={'space-between'}
+                  flexGrow={1}
+                >
+                  <Typography variant="body2" color="textPrimary">
+                    ${product.price}
+                  </Typography>
+                  <Button
+                    onClick={() => handleAddToCart(product)}
+                    size="small"
+                    color="primary"
+                  >
+                    Add To Cart
+                  </Button>
+                </Box>
+              </CardActions>
+            </Box>
           </Card>
         </Grid>
       ))}
